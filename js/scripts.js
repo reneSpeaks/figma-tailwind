e21eea0b91571efc78e50e6dc84c7468090d6efe
Nesrin D. Tanyeri <daghantanyeri@gmail.com>
2024-09-24 11:29:41 +0200

    new feature-thelocation

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          primary: "#BFD7B7",
+          secondary: "#BFD7B7",
+          accent: "#CDE895",
+          background: "#2F5651",
+        },
+        fontFamily: {
+          primary: ["Primary", "sans-serif"],
+          secondary: ["Secondary", "sans-serif"],
+        },
+      },
+    },
+    plugins: [],
+  };
+  
